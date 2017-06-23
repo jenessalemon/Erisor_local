9b9c49c29bb05bb656d4ff25f59e53dd6ffb90ef
Jenessa Lemon <jenessablotter12@yahoo.com>
2017-06-23 17:14:59 -0600

    Add files via upload
    Weird way to commit...

---
+++ Fix_str.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+str_file = "/Users/jimblotter/Desktop/Grad_School/Data_Analysis/QC/carol.str"
+
+# Import the .str file into pandas.
+IN_STR = pd.read_csv(str_file, sep="\t", na_values='-9', header=None)
+
+# This file has extra columns with no info. I remove them: axis=1 to remove column, how='all' to remove columns where ALL rows are blank.
+INN_STR = IN_STR.dropna(axis= 1, how= 'all')
+INN_STR = IN_STR.dropna(axis= 0, how= 'all')
+
+INN_STR.to_csv("/Users/jimblotter/Desktop/Grad_School/Data_Analysis/QC/paul.str", sep='\t',index= False,header= False)
+
+
+
+
+
+
+
+# na_values='-9' in the read_csv will get rid of nas but I need the whole line to be gone and stored elsewhere.
+
+#to get distance matrix:
+#read in erisor.str and import to pandas
+#remove extra columns with no info
+#remove lines with only -9, removing the ENTIRE line. store those lines in another file.
+
+#once I have my matrix:
+#meet with Aaron to write a crazy indexing code to get reps
+
+#replace blank with NaN then drop them again?
+#instead of dropping create another data frame that selects for them in the origional file.
+#name = name.fillna('-9')

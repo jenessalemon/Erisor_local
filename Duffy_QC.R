152e1675a949272b1430816be770ec0255e81e2a
Jenessa Lemon <jenessablotter12@yahoo.com>
2017-06-26 15:51:28 -0600

    Just progress

--- Duffy_QC.R
+++ Duffy_QC.R
@@ -23,15 +23,18 @@ ploidy(obj1) # should return 2 since we gave it 2 alleles for each marker DOES
 #Neighbor joining euclidian distance tree
 D <- dist(tab(obj1))
 
-#What does the matrix look like?
+#What does D look like?
 D
 dim(D)                             #NULL, Apparenty because this is a "dist" object.
 class(D)                           #dist
 attributes(D)
+str(D)
 D[1]                               #didnt get a 0 so that is good
+which(D==62.34581)                 #didn't work. integer(0)
+
 attributes(dist1)
-mat2 <- as(dist1, "matrix")
-mat2[1, 2] <- 0
+M <- as(D, "matrix")               #this maybe makes a normal matrix of the distance values?
+M                                  #there's a lot of 0s, but there are actually values appearing?
 
 #Indexing Distance Matrix (D)
 D

97228a7793c6abc95a6765ca5792dff685f9e543
Jenessa Lemon <jenessablotter12@yahoo.com>
2017-07-10 16:00:48 -0600

    regression

---
+++ loci_reads_regression.R
@@ -0,0 +1,7 @@
+setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/QC/')
+stats <- read.table("erisor_reps_stats.txt", header = TRUE, fill = TRUE)
+reads_raw <- stats[,2]
+loci_in_assembly <- stats[,9]
+
+reads_and_loci <- subset(stats, select = c("reads_raw", "loci_in_assembly"))
+summary(reads_and_loci)
